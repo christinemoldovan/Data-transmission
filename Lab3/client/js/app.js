3fc5dee5e2cf40ca03c3a619224dd1c8f4a4ad9a
Christine <christine_moldovan@yahoo.com>
2022-03-28 12:29:45 +0300

    Lab3 updated

--- Lab3/client/js/app.js
+++ Lab3/client/js/app.js
@@ -39,19 +39,7 @@ var app = new Vue({
             }
         },
         encode: function(bits){
-           /* var c4=this.parity(parseInt(bits[1].data)+parseInt(bits[2].data)+parseInt(bits[3].data)); // se calculeaza bitul de control de pe pozitia 4
-            var c2=this.parity(parseInt(bits[0].data)+parseInt(bits[2].data)+parseInt(bits[3].data)); // se calculeaza bitul de control de pe pozitia 2
-            var c1=this.parity(parseInt(bits[0].data)+parseInt(bits[1].data)+parseInt(bits[3].data)); // se calculeaza bitul de control de pe pozitia 1
-            // var C0 = this. ...
-            var c0=this.parity(parseInt(bits[0].data)+parseInt(bits[1].data)+parseInt(bits[2].data)+c1+c2+c4); //bitul de paritate de pe pozitia 0
-			console.log("Parity bit: c0:"+c0);
-            console.log("Control bits: "+"c1:"+c1+", c2:"+c2+", c4:"+c4);
-            return [c0,c1,c2,parseInt(bits[0].data),c4,parseInt(bits[1].data),parseInt(bits[2].data),parseInt(bits[3].data)]; // vectorul V (cuvantul de transmis)*/
-
-            // This function must be changed to allow any number of data bits
-            // Right now it only works for 4 data bits
-
-            if(bits.length == 4){
+                if(bits.length == 4){
             var c4=this.parity(parseInt(bits[1].data)+parseInt(bits[2].data)+parseInt(bits[3].data)); // se calculeaza bitul de control de pe pozitia 4
             var c2=this.parity(parseInt(bits[0].data)+parseInt(bits[2].data)+parseInt(bits[3].data)); // se calculeaza bitul de control de pe pozitia 2
             var c1=this.parity(parseInt(bits[0].data)+parseInt(bits[1].data)+parseInt(bits[3].data)); // se calculeaza bitul de control de pe pozitia 1
@@ -63,16 +51,16 @@ var app = new Vue({
             var v1 = [c0,c1,c2,parseInt(bits[0].data),c4,parseInt(bits[1].data),parseInt(bits[2].data),parseInt(bits[3].data)]; // outputing v1
             console.log("Vectorul v1 pentru 4 biti este: "+ v1);
             return [c0,c1,c2,parseInt(bits[0].data),c4,parseInt(bits[1].data),parseInt(bits[2].data),parseInt(bits[3].data)]; // vectorul V (cuvantul de transmis)
-        }
-        else{
-            console.log("Control bits: "+"c1:"+c1+", c2:"+c2+", c4:"+c4);
-            var v2 = [c1,c2,parseInt(bits[0].data),c4,parseInt(bits[1].data),parseInt(bits[2].data),parseInt(bits[3].data)] ;
-            console.log("Vectorul v2 pentru 4 biti este: "+ v2);
-            return [c1,c2,parseInt(bits[0].data),c4,parseInt(bits[1].data),parseInt(bits[2].data),parseInt(bits[3].data)]; // vectorul V (cuvantul de transmis)
-        }
-            
             }
-            else
+
+                else{
+                console.log("Control bits: "+"c1:"+c1+", c2:"+c2+", c4:"+c4);
+                var v2 = [c1,c2,parseInt(bits[0].data),c4,parseInt(bits[1].data),parseInt(bits[2].data),parseInt(bits[3].data)] ;
+                console.log("Vectorul v2 pentru 4 biti este: "+ v2);
+                return [c1,c2,parseInt(bits[0].data),c4,parseInt(bits[1].data),parseInt(bits[2].data),parseInt(bits[3].data)]; // vectorul V (cuvantul de transmis)
+                }
+            
+        }else
                 if(bits.length == 8){
                     var c8 = this.parity(parseInt(bits[4].data)+parseInt(bits[5].data)+parseInt(bits[6].data)+parseInt(bits[7].data));
                     var c4=this.parity(parseInt(bits[1].data)+parseInt(bits[2].data)+parseInt(bits[3].data)+parseInt(bits[7].data)); // se calculeaza bitul de control de pe pozitia 4
@@ -80,14 +68,15 @@ var app = new Vue({
                     var c1=this.parity(parseInt(bits[0].data)+parseInt(bits[1].data)+parseInt(bits[3].data)+parseInt(bits[5].data)+parseInt(bits[6].data)); // se calculeaza bitul de control de pe pozitia 1
                     var c0=this.parity(parseInt(bits[0].data) + parseInt(bits[1].data) + parseInt(bits[2].data) + parseInt(bits[3].data) + parseInt(bits[4].data) + parseInt(bits[5].data) 
                             + parseInt(bits[6].data) + parseInt(bits[7].data) + c1 +  c2 + c4+ c8);
-                  if(numberOfDataParity==0){
-                     console.log("Parity bit: c0:"+c0);
-                     console.log("Control bits: "+"c1:"+c1+", c2:"+c2+", c4:"+c4+", c8:"+c8);
-                     var v1 = [c0,c1,c2,parseInt(bits[0].data),c4,parseInt(bits[1].data),parseInt(bits[2].data),parseInt(bits[3].data),c8,parseInt(bits[4].data),
-                                parseInt(bits[5].data),parseInt(bits[6].data),parseInt(bits[7].data)]; // outputing v1
-                     console.log("Vectorul v1 pentru 8 biti este: "+ v1);
-                     return v1; // vectorul V (cuvantul de transmis)
-                }else{
+                      if(numberOfDataParity==0){
+                         console.log("Parity bit: c0:"+c0);
+                         console.log("Control bits: "+"c1:"+c1+", c2:"+c2+", c4:"+c4+", c8:"+c8);
+                         var v1 = [c0,c1,c2,parseInt(bits[0].data),c4,parseInt(bits[1].data),parseInt(bits[2].data),parseInt(bits[3].data),c8,parseInt(bits[4].data),
+                                    parseInt(bits[5].data),parseInt(bits[6].data),parseInt(bits[7].data)]; // outputing v1
+                         console.log("Vectorul v1 pentru 8 biti este: "+ v1);
+                         return v1; // vectorul V (cuvantul de transmis)
+                    }
+                    else{
                      console.log("Control bits: "+"c1:"+c1+", c2:"+c2+", c4:"+c4+", c8:"+c8);
                      var v2 = [c1,c2,parseInt(bits[0].data),c4,parseInt(bits[1].data),parseInt(bits[2].data),parseInt(bits[3].data),c8,parseInt(bits[4].data),
                                 parseInt(bits[5].data),parseInt(bits[6].data),parseInt(bits[7].data)]; // outputing v1
--- Lab3/server/hamming.js
+++ Lab3/server/hamming.js
@@ -3,21 +3,37 @@ function decode(bits) {
 	//fara paritate
 	if(bits.length==12){
 		var z8=parity(bits[1]+bits[2]+bits[3]+bits[4]);
-		//de 4 biti
 		var z4=parity(bits[3]+bits[4]+bits[5]+bits[6]);
 		var z2=parity(bits[1]+bits[2]+bits[5]+bits[6]);
 		var z1=parity(bits[0]+bits[2]+bits[4]+bits[6]);
+
+		//corectare eroare
+		var errorPosition=z1*1+z2*2+z4*4+z8*8;
+		var errorDetected=false;
+		if (errorPosition!=0) errorDetected=true;
+		if (errorDetected) {
+			bits[errorPosition-1]=parity(bits[errorPosition-1]+1);
+		}
+		return { errorCorrected: errorDetected, errorPosition: errorPosition-1, bits: bits };
 
 	}
 	//cu paritate
 	if(bits.length==13){
 		var z8=parity(bits[1]+bits[2]+bits[3]+bits[4]);
-
 		var z4=parity(bits[3]+bits[4]+bits[5]+bits[6]);
 		var z2=parity(bits[1]+bits[2]+bits[5]+bits[6]);
 		var z1=parity(bits[0]+bits[2]+bits[4]+bits[6]);
-		// cu paritate
 		var z0=parity(bits[0]+bits[1]+bits[2]+bits[3]+bits[4]+bits[5]+bits[6]+bits[7]+bits[8]);
+
+		//corectare eroare
+		var errorPosition=z1*1+z2*2+z4*4+z8*8;
+		var errorParity=z0;
+		var errorDetected=false;
+		if (errorPosition!=0 && errorParity!=0) errorDetected=true;
+		if (errorDetected) {
+			bits[errorPosition]=parity(bits[errorPosition]+1);
+		}
+		return { errorCorrected: errorDetected, errorPosition: errorPosition, bits: bits };
 	}
 
 	//de 4 biti
@@ -26,15 +42,32 @@ function decode(bits) {
 		var z4=parity(bits[3]+bits[4]+bits[5]+bits[6]);
 		var z2=parity(bits[1]+bits[2]+bits[5]+bits[6]);
 		var z1=parity(bits[0]+bits[2]+bits[4]+bits[6]);
-		
+
+		//corectare eroare
+		var errorPosition=z1*1+z2*2+z4*4;
+		var errorDetected=false;
+		if (errorPosition!=0) errorDetected=true;
+		if (errorDetected) {
+			bits[errorPosition-1]=parity(bits[errorPosition-1]+1);
+		}
+		return { errorCorrected: errorDetected, errorPosition: errorPosition-1, bits: bits };
 	}
 	//cu paritate
 	if(bits.length==8){
 		var z4=parity(bits[3]+bits[4]+bits[5]+bits[6]);
 		var z2=parity(bits[1]+bits[2]+bits[5]+bits[6]);
 		var z1=parity(bits[0]+bits[2]+bits[4]+bits[6]);
-		// cu paritate
 		var z0=parity(bits[0]+bits[1]+bits[2]+bits[3]+bits[4]+bits[5]+bits[6]+bits[7]+bits[8]);
+
+		//corectare eroare
+		var errorPosition=z1*1+z2*2+z4*4;
+		var errorParity=z0;
+		var errorDetected=false;
+		if (errorPosition!=0 && errorParity!=0) errorDetected=true;
+		if (errorDetected) {
+			bits[errorPosition]=parity(bits[errorPosition]+1);
+		}
+		return { errorCorrected: errorDetected, errorPosition: errorPosition, bits: bits };
 	}
     
     var errorPosition=z0*0+z1*1+z2*2+z4*4;
