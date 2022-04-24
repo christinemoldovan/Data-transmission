a3a7e61f6b9fe10f33f4701aea6a1b18c78b01bf
Christine <christine_moldovan@yahoo.com>
2022-04-24 15:51:02 +0300

    Lab5 encoding

---
+++ Lab5/encoder/js/app.js
@@ -0,0 +1,41 @@
+var app = new Vue({
+    el: '#baseband-encoder',
+    data: {
+        bits: [],
+        encodedBits: [],
+        encodedBits2: [],
+        encodedBits3: [],
+        encodedBits4: [],
+        encodedBits5: [],
+        encodedBits6: [],
+        encodedBits7: [],
+        encodedBits8: [],
+        encodedBits9: [],
+        status: '',
+        numberOfBits: 8,
+        validateBit: validateBit
+    },
+    created: function () {
+        this.bits = getBitstream(this.numberOfBits);
+    },
+    methods: {
+        encode: function(){   
+           // this.encodedBits = getManchesterLevelEncoding(this.bits);
+            this.encodedBits =  nonReturnToZeroLevel(this.bits);
+            this.encodedBits2 = nonReturnToZeroMark(this.bits);
+            this.encodedBits3 = nonReturnToZeroSpace(this.bits);
+            this.encodedBits4 = returnToZero(this.bits);
+            this.encodedBits5 = biphaseLevel(this.bits);
+            this.encodedBits6 = biphaseMark(this.bits);
+            this.encodedBits7 = biphaseSpace(this.bits);
+            this.encodedBits8 = biphaseLevelDifferential(this.bits);
+            this.encodedBits9 = biphaseDelay(this.bits);
+
+        
+            
+        }
+        
+    }
+})
+
+
---
+++ Lab5/encoder/js/baseband-codes.js
@@ -0,0 +1,179 @@
+/*function getManchesterLevelEncoding(bits) {
+    var result = [];
+    for (var i = 0; i < bits.length; i++) {
+        let symbol = '⚋⚋';
+        if (parseInt(bits[i].value) == 1) symbol = '▁∣▔';
+      //  AMI = "- 0 _" => - = 1; 0 = 0; _ = -1
+        if (parseInt(bits[i].value) == 1 && i > 0 && parseInt(bits[i - 1].value) == 1) symbol = '∣▁∣▔';
+        if (parseInt(bits[i].value) == 0) symbol = '▔∣▁';
+        if (parseInt(bits[i].value) == 0 && i > 0 && parseInt(bits[i - 1].value) == 0) symbol = '∣▔∣▁';
+        result.push(symbol);
+    }
+    return result;
+}*/
+
+function nonReturnToZeroLevel(bits) {
+    var result = [];
+    for (var i = 0; i < bits.length; i++) {
+        let symbol = '⚋';
+        if (parseInt(bits[i].value) == 1) symbol ='|▔';
+        if (parseInt(bits[i].value) == 1 && i > 0 && parseInt(bits[i - 1].value) == 1) symbol = '▔';
+        if (parseInt(bits[i].value) == 0) symbol = '|▁';
+        if (parseInt(bits[i].value) == 0 && i > 0 && parseInt(bits[i - 1].value) == 0) symbol = '▁';
+        result.push(symbol);
+    }
+    return result;
+}
+
+
+function nonReturnToZeroMark(bits) {
+    var result = [];
+    let level = 0;
+    for (var i = 0; i < bits.length; i++) {
+        let symbol = '⚋';
+        if (parseInt(bits[i].value) == 1 && level == 0) {
+            symbol = '|▔';
+            level = 1;
+        }
+        else if (parseInt(bits[i].value) == 1 && level == 1) {
+            symbol = '|▁';
+            level = 0;
+        }
+        if (parseInt(bits[i].value) == 0 && level == 0) symbol = '▁';
+        if (parseInt(bits[i].value) == 0 && level == 1) symbol = '▔';
+        result.push(symbol);
+    }
+    return result;
+}
+
+function nonReturnToZeroSpace(bits) {
+    var result = [];
+    let level = 0;
+    for (var i = 0; i < bits.length; i++) {
+        let symbol = '⚋';
+        if (parseInt(bits[i].value) == 0 && level == 0) {
+            symbol = '|▔';
+            level = 1;
+        }
+        else if (parseInt(bits[i].value) == 0 && level == 1) {
+            symbol = '|▁';
+            level = 0;
+        }
+        if (parseInt(bits[i].value) == 1 && level == 0) symbol = '▁';
+        if (parseInt(bits[i].value) == 1 && level == 1) symbol = '▔';
+        result.push(symbol);
+    }
+    return result;
+}
+
+function returnToZero(bits) {
+    var result = [];
+    for (var i = 0; i < bits.length; i++) {
+        let symbol = '⚋⚋';
+        if (parseInt(bits[i].value) == 1) symbol = '|▔|▁';
+        if (parseInt(bits[i].value) == 0) symbol = '▁';
+        result.push(symbol);
+    }
+    return result;
+}
+
+function biphaseLevel(bits) {
+    var result = [];
+    for (var i = 0; i < bits.length; i++) {
+        let symbol = '⚋';
+        if (parseInt(bits[i].value) == 1) symbol = '▔|▁';
+        if (parseInt(bits[i].value) == 1 && i > 0 && parseInt(bits[i - 1].value) == 1) symbol = '|▔|▁';
+        if (parseInt(bits[i].value) == 0) symbol = '▁|▔';
+        if (parseInt(bits[i].value) == 0 && i > 0 && parseInt(bits[i - 1].value) == 0) symbol = '|▁|▔';
+        result.push(symbol);
+    }
+    return result;
+}
+
+function biphaseMark(bits) {
+    var result = [];
+    let level = 0;
+    for (var i = 0; i < bits.length; i++) {
+        let symbol = '⚋';
+        if (parseInt(bits[i].value) == 0 && level == 0) {
+            symbol = '|▔';
+            level = 1;
+        }
+        else if (parseInt(bits[i].value) == 0 && level == 1) {
+            symbol = '|▁';
+            level = 0;
+        }
+        if (parseInt(bits[i].value) == 1 && level == 0) symbol = '|▔|▁';
+        if (parseInt(bits[i].value) == 1 && level == 1) symbol = '|▁|▔';
+        result.push(symbol);
+    }
+    return result;
+}
+
+function biphaseSpace(bits) {
+    var result = [];
+    let level = 0;
+    for (var i = 0; i < bits.length; i++) {
+        let symbol = '⚋';
+        if (parseInt(bits[i].value) == 1 && level == 0) {
+            symbol = '|▔';
+            level = 1;
+        }
+        else if (parseInt(bits[i].value) == 1 && level == 1) {
+            symbol = '|▁';
+            level = 0;
+        }
+        if (parseInt(bits[i].value) == 0 && level == 0) symbol = '|▔|▁';
+        if (parseInt(bits[i].value) == 0 && level == 1) symbol = '|▁|▔';
+        result.push(symbol);
+    }
+    return result;
+}
+
+function biphaseLevelDifferential(bits) {
+    var result = [];
+    let level = 0;
+    for (var i = 0; i < bits.length; i++) {
+        let symbol = '⚋';
+        if (parseInt(bits[i].value) == 1 && level == 0) {
+            symbol = '▁|▔';
+            level = 1;
+        }
+        else if (parseInt(bits[i].value) == 1 && level == 1) {
+            symbol = '▔|▁';
+            level = 0;
+        }
+        if (parseInt(bits[i].value) == 0 && level == 1) symbol = '|▁|▔';
+        if (parseInt(bits[i].value) == 0 && level == 0) symbol = '|▔|▁';
+        result.push(symbol);
+    }
+    return result;
+}
+
+function biphaseDelay(bits) {
+    var result = [];
+    let level = 0;
+    for (var i = 0; i < bits.length; i++) {
+        let symbol = '⚋';
+        if (parseInt(bits[i].value) == 1 && level == 0) {
+            symbol = '▁|▔';
+            level = 1;
+        }
+        else if (parseInt(bits[i].value) == 1 && level == 1) {
+            symbol = '▔|▁';
+            level = 0;
+        }
+        if (parseInt(bits[i].value) == 0 && level == 1 && i > 0 && parseInt(bits[i-1].value) == 1) symbol = '▔';
+        else if(parseInt(bits[i].value) == 0 && level == 1 && i > 0 && parseInt(bits[i-1].value) == 0) {
+            symbol = '|▁';
+            level = 0;
+        }
+        else if (parseInt(bits[i].value) == 0 && level == 0 && i > 0 && parseInt(bits[i-1].value) == 1) symbol = '▁';
+        else if(parseInt(bits[i].value) == 0 && level == 0 && i > 0 && parseInt(bits[i-1].value) == 0) {
+            symbol = '|▔';
+            level = 1;
+        }
+        result.push(symbol);
+    }
+    return result;
+}
